a53599d488a6fd41b0bbec98d7953dc33eac9e7f
Farin Lippmann <farin.lippmann@uni-jena.de>
2022-05-12 09:01:08 +0200

    Program: Small refactoring

--- program/src/differentiator.py
+++ program/src/differentiator.py
@@ -30,115 +30,131 @@ class Differentiator():
         self.diffDag.set_tensorrank(self.variable_ranks, self.arg)
 
     def reverse_mode_diff(self, node, diff):  # Computes derivative of node.left and node.right | node: node in original dag | diff : node that contains derivative with respect to node.
-        currentDiffNode = diff
-        # PRODUCT
         if node.type == NODETYPE.PRODUCT:
-            s1 = node.leftIndices
-            s2 = node.rightIndices
-            s3 = node.resultIndices
-            s4 = ''.join([i for i in string.ascii_lowercase if i not in (s1 + s2 + s3)][0:self.originalDag.rank])   # Use some unused indices for the output node y
-            if node.left and node.left.contains(self.arg):
-                diff = Tree(NODETYPE.PRODUCT, f'*({s4+s3},{s2}->{s4+s1})', currentDiffNode, node.right)   # Diff rule
-                diff.set_indices(s4+s3, s2, s4+s1)
-                diff = self.contributions(node.left, diff)
-            if node.right and node.right.contains(self.arg):
-                diff = Tree(NODETYPE.PRODUCT, f'*({s4+s3},{s1}->{s4+s2})', currentDiffNode, node.left)
-                diff.set_indices(s4+s3, s1, s4+s2)
-                diff = self.contributions(node.right, diff)
-        # SUM
+            diff = self.diff_product(node, diff)
         elif node.type == NODETYPE.SUM:
-            if node.left and node.left.contains(self.arg):
-                diff = self.contributions(node.left, currentDiffNode)
-            if node.right and node.right.contains(self.arg):
-                diff = self.contributions(node.right, currentDiffNode)
-        # POWER
+            diff = self.diff_sum(node, diff)
         elif node.type == NODETYPE.POWER:
-            if node.left and node.right and node.left.contains(self.arg) and node.right.contains(self.arg):
-                raise Exception('Encountered power node with argument in left and right operands during differentiation.')
-            if node.left and node.left.contains(self.arg):
-                indices = ''.join([i for i in string.ascii_lowercase][0:node.left.rank])
-                one = Tree(NODETYPE.VARIABLE, '_ones(0)')
-                self.variable_ranks[f'_ones(0)'] = 0
-                newpower = Tree(NODETYPE.POWER, '^', node.left, Tree(NODETYPE.SUM, '+', node.right, Tree(NODETYPE.ELEMENTWISE_FUNCTION, '-', None, one)))
-                funcDiff = Tree(NODETYPE.PRODUCT, f'*(,{indices}->{indices})', node.right, newpower)
-                funcDiff.set_indices('', indices, indices)
-                s1 = ''.join(string.ascii_lowercase[0:node.left.rank])   # Same procedure as with an elementwise function
-                s2 = ''.join([i for i in string.ascii_lowercase if i not in s1][0:self.originalDag.rank])
-                diff = Tree(NODETYPE.PRODUCT, f'*({s2+s1},{s1}->{s2+s1})', diff, funcDiff) # Diff rule
-                diff.set_indices(s2+s1, s1, s2+s1)
-                diff = self.contributions(node.left, diff)
-            elif node.right and node.right.contains(self.arg):
-                s3 = ''.join([i for i in string.ascii_lowercase][0:self.originalDag.rank])
-                s2 = ''.join([i for i in string.ascii_lowercase if not i in s3][0:node.rank])
-                s1 = ''
-                funcDiff = Tree(NODETYPE.PRODUCT, f'*({s2},{s2}->{s2})', node, Tree(NODETYPE.ELEMENTWISE_FUNCTION, 'log', None, node.left))
-                funcDiff.set_indices(s2, s2, s2)
-                diff = Tree(NODETYPE.PRODUCT, f'*({s3+s2},{s2+s1}->{s3+s1})', diff, funcDiff)
-                diff.set_indices(s3+s2, s2+s1, s3+s1)
-                diff = self.contributions(node.right, diff)
-        # ELEMENTWISE FUNCTION
+            diff = self.diff_power(node, diff)
         elif node.type == NODETYPE.ELEMENTWISE_FUNCTION:
-            if node.name == '-':
-                ones_rank = node.right.rank
-                funcDiff = Tree(NODETYPE.ELEMENTWISE_FUNCTION, '-', None, Tree(NODETYPE.VARIABLE, f'_ones({ones_rank})'))
-                self.variable_ranks[f'_ones({ones_rank})'] = ones_rank
-            elif node.name == 'sin':
-                funcDiff = Tree(NODETYPE.ELEMENTWISE_FUNCTION, 'cos', None, node.right)
-            elif node.name == 'cos':
-                funcDiff = Tree(NODETYPE.ELEMENTWISE_FUNCTION, '-', None, Tree(NODETYPE.ELEMENTWISE_FUNCTION, 'sin', None, node.right))
-            elif node.name == 'tan':
-                cos = Tree(NODETYPE.ELEMENTWISE_FUNCTION, 'cos', None, node.right)
-                indices = ''.join([i for i in string.ascii_lowercase][0:node.right.rank])
-                cos_squared = Tree(NODETYPE.PRODUCT, f'*({indices},{indices}->{indices})', cos, cos)
-                cos_squared.set_indices(indices, indices, indices)
-                funcDiff = Tree(NODETYPE.ELEMENTWISE_FUNCTION, 'elementwise_inverse', None, cos_squared)
-            elif node.name == 'arctan':
-                indices = ''.join([i for i in string.ascii_lowercase][0:node.right.rank])
-                squared = Tree(NODETYPE.PRODUCT, f'*({indices},{indices}->{indices})', node.right, node.right)
-                squared.set_indices(indices, indices, indices)
-                ones_rank = node.right.rank
-                funcDiff = Tree(NODETYPE.ELEMENTWISE_FUNCTION, 'elementwise_inverse', None, Tree(NODETYPE.SUM, '+', squared, Tree(NODETYPE.VARIABLE, f'_ones({ones_rank})')))
-                self.variable_ranks[f'_ones({ones_rank})'] = ones_rank
-            elif node.name == 'exp':
-                funcDiff = Tree(NODETYPE.ELEMENTWISE_FUNCTION, 'exp', None, node.right)
-            elif node.name == 'log':
-                funcDiff = Tree(NODETYPE.ELEMENTWISE_FUNCTION, 'elementwise_inverse', None, node.right)
-            elif node.name == 'tanh':
-                indices = ''.join([i for i in string.ascii_lowercase][0:node.right.rank])
-                squared = Tree(NODETYPE.PRODUCT, f'*({indices},{indices}->{indices})', node, node)
-                squared.set_indices(indices, indices, indices)
-                ones_rank = node.right.rank
-                funcDiff = Tree(NODETYPE.SUM, '+', Tree(NODETYPE.VARIABLE, f'_ones({ones_rank})'), Tree(NODETYPE.ELEMENTWISE_FUNCTION, '-', None, squared))
-                self.variable_ranks[f'_ones({ones_rank})'] = ones_rank
-            elif node.name == 'abs':
-                funcDiff = Tree(NODETYPE.ELEMENTWISE_FUNCTION, 'sign', None, node.right)
-            elif node.name == 'sign':
-                zeroes_rank = node.right.rank
-                funcDiff = Tree(NODETYPE.VARIABLE, f'_zeroes({zeroes_rank})')
-                self.variable_ranks[f'_zeroes({zeroes_rank})'] = zeroes_rank
-            elif node.name == 'relu':
-                funcDiff = Tree(NODETYPE.ELEMENTWISE_FUNCTION, 'relu', None, Tree(NODETYPE.ELEMENTWISE_FUNCTION, 'sign', None, node.right))
-            elif node.name == 'elementwise_inverse':
-                indices = ''.join([i for i in string.ascii_lowercase][0:node.right.rank])
-                squared = Tree(NODETYPE.PRODUCT, f'*({indices},{indices}->{indices})', node.right, node.right)
-                squared.set_indices(indices, indices, indices)
-                funcDiff = Tree(NODETYPE.ELEMENTWISE_FUNCTION, '-', None, Tree(NODETYPE.ELEMENTWISE_FUNCTION, 'elementwise_inverse', None, squared))
-            else:
-                raise Exception(f'Unknown function {node.name} encountered during differentiation.')
-            s1 = ''.join(string.ascii_lowercase[0:node.right.rank])
+            diff = self.diff_elementwise_function(node, diff)
+        elif node.type == NODETYPE.VARIABLE:
+            diff = self.diff_variable(node, diff)
+        return diff
+    
+    def diff_product(self, node, diff):
+        currentDiffNode = diff
+        s1 = node.leftIndices
+        s2 = node.rightIndices
+        s3 = node.resultIndices
+        s4 = ''.join([i for i in string.ascii_lowercase if i not in (s1 + s2 + s3)][0:self.originalDag.rank])   # Use some unused indices for the output node y
+        if node.left and node.left.contains(self.arg):
+            diff = Tree(NODETYPE.PRODUCT, f'*({s4+s3},{s2}->{s4+s1})', currentDiffNode, node.right)   # Diff rule
+            diff.set_indices(s4+s3, s2, s4+s1)
+            diff = self.contributions(node.left, diff)
+        if node.right and node.right.contains(self.arg):
+            diff = Tree(NODETYPE.PRODUCT, f'*({s4+s3},{s1}->{s4+s2})', currentDiffNode, node.left)
+            diff.set_indices(s4+s3, s1, s4+s2)
+            diff = self.contributions(node.right, diff)
+        return diff
+    
+    def diff_sum(self, node, diff):
+        currentDiffNode = diff
+        if node.left and node.left.contains(self.arg):
+            diff = self.contributions(node.left, currentDiffNode)
+        if node.right and node.right.contains(self.arg):
+            diff = self.contributions(node.right, currentDiffNode)
+        return diff
+    
+    def diff_power(self, node, diff):
+        if node.left and node.right and node.left.contains(self.arg) and node.right.contains(self.arg):
+            raise Exception('Encountered power node with argument in left and right operands during differentiation.')  # This case is handled by a previous transform of the expression
+        if node.left and node.left.contains(self.arg):
+            indices = ''.join([i for i in string.ascii_lowercase][0:node.left.rank])
+            one = Tree(NODETYPE.VARIABLE, '_ones(0)')
+            self.variable_ranks[f'_ones(0)'] = 0
+            newpower = Tree(NODETYPE.POWER, '^', node.left, Tree(NODETYPE.SUM, '+', node.right, Tree(NODETYPE.ELEMENTWISE_FUNCTION, '-', None, one)))
+            funcDiff = Tree(NODETYPE.PRODUCT, f'*(,{indices}->{indices})', node.right, newpower)
+            funcDiff.set_indices('', indices, indices)
+            s1 = ''.join(string.ascii_lowercase[0:node.left.rank])   # Same procedure as with an elementwise function
             s2 = ''.join([i for i in string.ascii_lowercase if i not in s1][0:self.originalDag.rank])
             diff = Tree(NODETYPE.PRODUCT, f'*({s2+s1},{s1}->{s2+s1})', diff, funcDiff) # Diff rule
             diff.set_indices(s2+s1, s1, s2+s1)
-            if node.right and node.right.contains(self.arg):
-                diff = self.contributions(node.right, diff)
-        # VARIABLE
-        elif node.type == NODETYPE.VARIABLE:
-            if node == self.arg:
-                pass
-            else:
-                raise Exception('Reached non-argument variable during differentiation.')
+            diff = self.contributions(node.left, diff)
+        elif node.right and node.right.contains(self.arg):
+            s3 = ''.join([i for i in string.ascii_lowercase][0:self.originalDag.rank])
+            s2 = ''.join([i for i in string.ascii_lowercase if not i in s3][0:node.rank])
+            s1 = ''
+            funcDiff = Tree(NODETYPE.PRODUCT, f'*({s2},{s2}->{s2})', node, Tree(NODETYPE.ELEMENTWISE_FUNCTION, 'log', None, node.left))
+            funcDiff.set_indices(s2, s2, s2)
+            diff = Tree(NODETYPE.PRODUCT, f'*({s3+s2},{s2+s1}->{s3+s1})', diff, funcDiff)
+            diff.set_indices(s3+s2, s2+s1, s3+s1)
+            diff = self.contributions(node.right, diff)
         return diff
-
+    
+    def diff_elementwise_function(self, node, diff):
+        if node.name == '-':
+            ones_rank = node.right.rank
+            funcDiff = Tree(NODETYPE.ELEMENTWISE_FUNCTION, '-', None, Tree(NODETYPE.VARIABLE, f'_ones({ones_rank})'))
+            self.variable_ranks[f'_ones({ones_rank})'] = ones_rank
+        elif node.name == 'sin':
+            funcDiff = Tree(NODETYPE.ELEMENTWISE_FUNCTION, 'cos', None, node.right)
+        elif node.name == 'cos':
+            funcDiff = Tree(NODETYPE.ELEMENTWISE_FUNCTION, '-', None, Tree(NODETYPE.ELEMENTWISE_FUNCTION, 'sin', None, node.right))
+        elif node.name == 'tan':
+            cos = Tree(NODETYPE.ELEMENTWISE_FUNCTION, 'cos', None, node.right)
+            indices = ''.join([i for i in string.ascii_lowercase][0:node.right.rank])
+            cos_squared = Tree(NODETYPE.PRODUCT, f'*({indices},{indices}->{indices})', cos, cos)
+            cos_squared.set_indices(indices, indices, indices)
+            funcDiff = Tree(NODETYPE.ELEMENTWISE_FUNCTION, 'elementwise_inverse', None, cos_squared)
+        elif node.name == 'arctan':
+            indices = ''.join([i for i in string.ascii_lowercase][0:node.right.rank])
+            squared = Tree(NODETYPE.PRODUCT, f'*({indices},{indices}->{indices})', node.right, node.right)
+            squared.set_indices(indices, indices, indices)
+            ones_rank = node.right.rank
+            funcDiff = Tree(NODETYPE.ELEMENTWISE_FUNCTION, 'elementwise_inverse', None, Tree(NODETYPE.SUM, '+', squared, Tree(NODETYPE.VARIABLE, f'_ones({ones_rank})')))
+            self.variable_ranks[f'_ones({ones_rank})'] = ones_rank
+        elif node.name == 'exp':
+            funcDiff = Tree(NODETYPE.ELEMENTWISE_FUNCTION, 'exp', None, node.right)
+        elif node.name == 'log':
+            funcDiff = Tree(NODETYPE.ELEMENTWISE_FUNCTION, 'elementwise_inverse', None, node.right)
+        elif node.name == 'tanh':
+            indices = ''.join([i for i in string.ascii_lowercase][0:node.right.rank])
+            squared = Tree(NODETYPE.PRODUCT, f'*({indices},{indices}->{indices})', node, node)
+            squared.set_indices(indices, indices, indices)
+            ones_rank = node.right.rank
+            funcDiff = Tree(NODETYPE.SUM, '+', Tree(NODETYPE.VARIABLE, f'_ones({ones_rank})'), Tree(NODETYPE.ELEMENTWISE_FUNCTION, '-', None, squared))
+            self.variable_ranks[f'_ones({ones_rank})'] = ones_rank
+        elif node.name == 'abs':
+            funcDiff = Tree(NODETYPE.ELEMENTWISE_FUNCTION, 'sign', None, node.right)
+        elif node.name == 'sign':
+            zeroes_rank = node.right.rank
+            funcDiff = Tree(NODETYPE.VARIABLE, f'_zeroes({zeroes_rank})')
+            self.variable_ranks[f'_zeroes({zeroes_rank})'] = zeroes_rank
+        elif node.name == 'relu':
+            funcDiff = Tree(NODETYPE.ELEMENTWISE_FUNCTION, 'relu', None, Tree(NODETYPE.ELEMENTWISE_FUNCTION, 'sign', None, node.right))
+        elif node.name == 'elementwise_inverse':
+            indices = ''.join([i for i in string.ascii_lowercase][0:node.right.rank])
+            squared = Tree(NODETYPE.PRODUCT, f'*({indices},{indices}->{indices})', node.right, node.right)
+            squared.set_indices(indices, indices, indices)
+            funcDiff = Tree(NODETYPE.ELEMENTWISE_FUNCTION, '-', None, Tree(NODETYPE.ELEMENTWISE_FUNCTION, 'elementwise_inverse', None, squared))
+        else:
+            raise Exception(f'Unknown function {node.name} encountered during differentiation.')
+        s1 = ''.join(string.ascii_lowercase[0:node.right.rank])
+        s2 = ''.join([i for i in string.ascii_lowercase if i not in s1][0:self.originalDag.rank])
+        diff = Tree(NODETYPE.PRODUCT, f'*({s2+s1},{s1}->{s2+s1})', diff, funcDiff) # Diff rule
+        diff.set_indices(s2+s1, s1, s2+s1)
+        if node.right and node.right.contains(self.arg):
+            diff = self.contributions(node.right, diff)
+        return diff
+    
+    def diff_variable(self, node, diff):
+        if node == self.arg:
+            pass
+        else:
+            raise Exception('Reached non-argument variable during differentiation.')
+        return diff
+        
     def contributions(self, node, diff):
         if node in self.originalNodeToDiffNode:   # If we've been to this node before, we need to add the new contribution to the old one
             diff = Tree(NODETYPE.SUM, '+', self.originalNodeToDiffNode[node], diff)
