c7636d517915f3ce277cf7afd382cd5c967ee41c
toucanmeister <45033744+toucanmeister@users.noreply.github.com>
2023-04-26 14:45:25 +0200

    made example in demo.py clearer

--- program/src/demo.py
+++ program/src/demo.py
@@ -4,7 +4,9 @@ from numcheck import numcheck
 
 if __name__ == '__main__':
     example= '''
-        declare X 2 expression 1*(,ij->)X derivative wrt X
+        declare X 2 
+        expression 1*(,ij->)X 
+        derivative wrt X
     '''
     diffDag, originalDag, arg_name, variable_ranks = differentiate(example)
     numcheck(originalDag, diffDag, variable_ranks, arg_name, verbose=True)
